90219f362d6b06263d59138bec90a9f0f6ff44d1
Recycledj98 <catalin.sonca.a@gmail.com>
2026-05-31 20:05:51 +0200

    chore: bump version 1.6.0+22 para prueba auto-update

--- lib/build_info.dart
+++ lib/build_info.dart
@@ -1,3 +1,3 @@
 // Regenerado automáticamente en cada build por codemagic.yaml
 // Para builds locales se mantiene la fecha del último commit
-const String kBuildDate = '2026-05-31';
+const String kBuildDate = '2026-06-01';
